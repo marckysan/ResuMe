f4d41918635091a8b26fd666a4e22639fd4232ef
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-12-08 17:43:46 +0800

    Implement checking of arguments for AchievementSelector

--- production/logic/AchievementSelector.java
+++ production/logic/AchievementSelector.java
@@ -1,9 +1,13 @@
 package production.logic;
 
+import static production.util.ArgumentChecker.checkArgument;
+
 public class AchievementSelector {
 
     private final int MIN_ACHIEVEMENT_INDEX = 0;
     private final int MAX_ACHIEVEMENT_INDEX;
+
+    private final String INVALID_ACHIEVEMENT_INDEX_MESSAGE = "Invalid index received!";
 
     private final AchievementSelection selection;
 
@@ -17,11 +21,17 @@ public class AchievementSelector {
     }
 
     public void select(int index) {
+        checkArgument(isValidIndex(index), INVALID_ACHIEVEMENT_INDEX_MESSAGE);
         selection.add(index);
     }
 
     public void deselect(int index) {
+        checkArgument(isValidIndex(index), INVALID_ACHIEVEMENT_INDEX_MESSAGE);
         selection.remove(index);
+    }
+
+    public boolean isValidIndex(int index) {
+        return index >= MAX_ACHIEVEMENT_INDEX && index <= MAX_ACHIEVEMENT_INDEX;
     }
 
 }
