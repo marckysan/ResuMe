9443dc997725b0f4d764a9a7bbfd204d1ff2780a
Marckysan <>
2020-12-17 01:05:57 +0800

    First commit webapp code

---
+++ WebApp-Code/app.js
@@ -0,0 +1,52 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const flash = require("connect-flash");
+const cookieParser = require('cookie-parser');
+const session = require('express-session'); // Allow storing of session data
+const passport = require("passport"); // Authentication of user
+var passportinit = require("./passportinit");
+
+
+// Files for the Routes
+var homeRouter = require('./routes/home');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(cookieParser());
+app.use(session({
+  secret: "secret",
+  saveUninitialized: true,
+  resave: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passportinit();
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.message = req.flash("message");
+  req.flash("message");
+  next();
+});
+
+
+// Routes
+app.use('/', homeRouter);
+
+
+app.listen(process.env.PORT || 3030, function() {
+  console.log("Server started on port 3030")
+});
